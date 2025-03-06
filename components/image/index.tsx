bab560da9ac85a5951af70622d75a7132e68cae8
codercarl1243 <codercarl1243@gmail.com>
2025-03-06 11:12:20 +1000

    cleanup

--- components/image/index.tsx
+++
@@ -1,9 +0,0 @@
-import { urlFor } from "@/sanity/lib/image";
-import ImageComponent from "./image.component";
-import type { ImageProps } from "./image";
-
-
-export default function Image({asset, alt, lqip}: ImageProps){
-
-    return <ImageComponent src={urlFor(asset).url()} alt={alt} lqip={lqip} />
-}
